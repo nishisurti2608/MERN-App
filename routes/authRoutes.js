f7187a50078b444eda6a225ea29bd2e3a2b3aca0
nishi <01nishisurti@gmail.com>
2023-10-25 18:21:21 -0400

    Login API created

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,18 @@
 import express from "express";
-import { registerController } from "../controller/authController.js";
+import { registerController,loginController } from "../controller/authController.js";
 
 //router object
 
 const router = express.Router();
 
 //routing
+
 //Register method POST
 
 router.post("/register", registerController);
+
+//Login method POST
+
+router.post("/login", loginController);
 
 export default router;
